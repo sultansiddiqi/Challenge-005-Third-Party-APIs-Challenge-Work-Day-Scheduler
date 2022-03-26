75a191b8b0f47e15a84d404f980da6d028e265ea
Sultan Siddiqi <sultan.siddiqi@gmail.com>
2022-03-26 12:15:39 -0400

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    //display current day & time.
+   var span = document.getElementById('currentDay');
+
+   function time() {
+     var d = new Date();
+     span.textContent = d 
+   }
+   setInterval(time, 1000);
+
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".task").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+        window.alert('Your task has been saved!');
+        console.log('The button has been clicked!');
+    })
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#hour7 .task").val(localStorage.getItem("hour7"));
+    $("#hour8 .task").val(localStorage.getItem("hour8"));
+    $("#hour9 .task").val(localStorage.getItem("hour9"));
+    $("#hour10 .task").val(localStorage.getItem("hour10"));
+    $("#hour11 .task").val(localStorage.getItem("hour11"));
+    $("#hour12 .task").val(localStorage.getItem("hour12"));
+    $("#hour13 .task").val(localStorage.getItem("hour13"));
+    $("#hour14 .task").val(localStorage.getItem("hour14"));
+    $("#hour15 .task").val(localStorage.getItem("hour15"));
+    $("#hour16 .task").val(localStorage.getItem("hour16"));
+    $("#hour17 .task").val(localStorage.getItem("hour17"));
+    $("#hour18 .task").val(localStorage.getItem("hour18"));
+    $("#hour19 .task").val(localStorage.getItem("hour19"));
+
+
+    function hourTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            //check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
+})
+
